798a43abbcbcdad3c9ec176d840112f6d64519f8
Fewing <yangzheyu98@gmail.com>
2018-11-28 23:24:40 +0800

    数据库测试

--- miniprogram/pages/databaseGuide/databaseGuide.js
+++ miniprogram/pages/databaseGuide/databaseGuide.js
@@ -21,30 +21,30 @@ Page({
   },
 
   onAdd: function () {
-    // const db = wx.cloud.database()
-    // db.collection('counters').add({
-    //   data: {
-    //     count: 1
-    //   },
-    //   success: res => {
-    //     // 在返回结果中会包含新创建的记录的 _id
-    //     this.setData({
-    //       counterId: res._id,
-    //       count: 1
-    //     })
-    //     wx.showToast({
-    //       title: '新增记录成功',
-    //     })
-    //     console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
-    //   },
-    //   fail: err => {
-    //     wx.showToast({
-    //       icon: 'none',
-    //       title: '新增记录失败'
-    //     })
-    //     console.error('[数据库] [新增记录] 失败：', err)
-    //   }
-    // })
+     const db = wx.cloud.database()
+     db.collection('counters').add({
+       data: {
+         count: 1
+       },
+       success: res => {
+          在返回结果中会包含新创建的记录的_id
+         this.setData({
+           counterId: res._id,
+           count: 1
+         })
+         wx.showToast({
+           title: '新增记录成功',
+         })
+         console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+       },
+       fail: err => {
+         wx.showToast({
+           icon: 'none',
+           title: '新增记录失败'
+         })
+         console.error('[数据库] [新增记录] 失败：', err)
+       }
+     })
   },
 
   onQuery: function() {
