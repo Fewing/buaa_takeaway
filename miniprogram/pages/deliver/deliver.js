5227ed9ff36eb43a3dafe377f4fcc4aa71334c54
Fewing <yangzheyu98@gmail.com>
2019-08-26 20:13:40 +0800

    添加下拉刷新功能

--- miniprogram/pages/deliver/deliver.js
+++ miniprogram/pages/deliver/deliver.js
@@ -148,7 +148,8 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
+    this.onShow()
+    wx.stopPullDownRefresh()
   },
 
   /**
--- miniprogram/pages/my_info/my_info.js
+++ miniprogram/pages/my_info/my_info.js
@@ -257,7 +257,8 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
+    this.onShow()
+    wx.stopPullDownRefresh()
   },
 
   /**
